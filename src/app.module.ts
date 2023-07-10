6981325e61fc8dda54061020a7e65b52fad744ea
chander1411 <leonardotoala1411@gmail.com>
2023-07-10 10:23:42 -0500

    major: test commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PracticaModule } from './practica/practica.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [PracticaModule, UsuariosModule],
+  imports: [PracticaModule, UsuariosModule, TestModule],
   controllers: [AppController],
   providers: [AppService],
 })
